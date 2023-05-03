be7850d078d35588a6a9e81ec567c9cb33bb0fe7
mizonov <mizonov.iv@gmail.com>
2023-05-03 17:17:05 +0500

    Navbar+Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Navbar/>
+
+  <section class="container mx-auto h-screen px-40 border-2 flex flex-col justify-end">
+    <header>
+      <h1 class="text-7xl font-bold">Юридическая компания в Екатеринбурге</h1>
+      <p class="text-4xl mt-4">Обеспечим полную защиту ваших интересов</p>
+    </header>
+    <main>
+      <ul class="flex justify-between mt-10 mb-10">
+        <li class="w-1/8 text-center" v-for="item in numbers">
+          <p class="m-2">{{item.title}}</p>
+          <hr>
+          <p class="m-2">{{item.subtitle}}</p>
+        </li>
+      </ul>
+    </main>
+  </section>
+
+  <section class="container mx-auto px-40">
+    <ul class="flex justify-center">
+      <li class="mx-4 px-10 border-2" v-for="item in services">
+        <h2>{{item.title}}</h2>
+        <div>
+          <p>{{item.subtitle}}</p>
+        </div>
+      </li>
+    </ul>
+  </section>
+
+
+
+  <section class="text-center border-2">
+    <h2>Footer</h2>
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import {ref} from "vue";
+
+const numbers = ref([
+  {
+    title: "17 000",
+    subtitle: "Банкротов по РФ"
+  },
+  {
+    title: "34",
+    subtitle: "Филиала по РФ"
+  },
+  {
+    title: "300",
+    subtitle: "Юристов-экспертов в области банкротства"
+  },
+  {
+    title: "50",
+    subtitle: "Лучших арбитражных управляющих"
+  }
+])
+
+const services = ref([
+  {
+    title: "Услуги для физических лиц",
+    subtitle: "Банкротство физических лиц"
+  },
+  {
+    title: "Услуги для юридических лиц",
+    subtitle: "Ликвидация компаний"
+  },
+])
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container mx-auto text-center border-2 px-40 py-4 fixed top-0 left-0">
+    <nav class="flex justify-between">
+      <a href="" class="text-xl">
+        Логотип
+      </a>
+      <ul class="flex">
+        <li class="p-2">
+          <a href="">Услуги</a>
+        </li>
+        <li class="p-2">
+          <a href="">О нас</a>
+        </li>
+        <li class="p-2">
+          <a href="">Отзывы</a>
+        </li>
+        <li class="p-2">
+          <a href="">Контакты</a>
+        </li>
+      </ul>
+      <button class="block w-60 p-1 bg-slate-400 text-white rounded">Получить <br> консультацию</button>
+    </nav>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
