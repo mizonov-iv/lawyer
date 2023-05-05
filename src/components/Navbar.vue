90c989749e1461fc75b06e83af32ac6b6b62564f
mizonov <mizonov.iv@gmail.com>
2023-05-05 14:54:38 +0500

    burger update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,23 @@
 <template>
-  <section class="container mx-auto text-center border-2 px-40 py-4 fixed top-0 left-0">
-    <nav class="flex justify-between">
+  <section class="container mx-auto text-center border-2 md:px-40 py-4 fixed top-0 left-0 right-0">
+    <nav class="flex justify-between px-4">
+      <div class="md:hidden z-20" v-if="!burgerToggle" @click.prevent="burgerToggle = true">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
+      <div class="md:hidden z-20" v-if="burgerToggle" @click.prevent="burgerToggle = false">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </div>
       <a href="" class="text-xl">
         Логотип
       </a>
-      <ul class="flex">
+      <ul
+          class=" w-3/4 h-screen absolute top-0 bg-slate-50 left-0 md:static md:flex z-10 border-2"
+          :class="burgerToggle ? 'block' : 'hidden'"
+      >
         <li class="p-2">
           <a href="">Услуги</a>
         </li>
@@ -18,15 +31,15 @@
           <a href="">Контакты</a>
         </li>
       </ul>
-      <button class="block w-60 p-1 bg-slate-400 text-white rounded">Получить <br> консультацию</button>
+      <button class="block w-20 md:w-60 p-1 bg-slate-400 text-white rounded">Получить консультацию</button>
     </nav>
   </section>
 </template>
 
-<script>
-export default {
+<script setup>
+import {ref} from "vue";
 
-}
+const burgerToggle = ref(false)
 </script>
 
 <style scoped>
