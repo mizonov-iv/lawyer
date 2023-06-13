233bc5c6f702c920ad54ec5826699b956a70ecb7
mizonov-iv <mizonov.iv@gmail.com>
2023-06-13 16:37:56 +0500

    navbar update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container mx-auto px-4 md:px-40 text-center py-4">
+  <section class="container mx-auto px-4 md:px-5 lg:px-4 text-center py-4">
     <nav class="flex justify-between px-4">
       <div class="md:hidden z-20" v-if="!burgerToggle" @click.prevent="burgerToggle = true">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -15,7 +15,7 @@
         <img class="w-32" src="/logo.png" alt="logo">
       </a>
         <ul
-            class=" w-3/4 md:w-6/12 h-screen py-10 sm:py-0 md:h-fit absolute top-0 left-0 md:static md:flex z-10 bg-gray-300 sm:bg-inherit"
+            class=" w-3/4 lg:w-6/12 h-screen py-10 sm:py-0 md:h-fit absolute top-0 left-0 md:static md:flex z-10 bg-gray-300 sm:bg-inherit"
             :class="burgerToggle ? 'block' : 'hidden'"
         >
           <li class="p-3 hover:scale-110 transition duration-700 ease-in-out">
@@ -39,7 +39,7 @@
           </li>
         </ul>
       <button
-          class="block w-32 md:w-60 p-1 bg-slate-400 text-white rounded hover:scale-110 transition duration-700 ease-in-out drop-shadow-2xl"
+          class="block w-32 md:w-60 md:p-0 p-1 bg-slate-400 text-white rounded hover:scale-110 transition duration-700 ease-in-out drop-shadow-2xl"
           @click="openContactModal"
       >
         Получить консультацию
